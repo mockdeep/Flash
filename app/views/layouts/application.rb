6d38269803b828b23d78967a2b353bddee0e8646
Robert Fletcher <lobatifricha@gmail.com>
2026-03-02 20:23:56 -0800

    add favicon link tags (#132)

--- app/views/layouts/application.rb
+++ app/views/layouts/application.rb
@@ -22,6 +22,9 @@ module Views
             csrf_meta_tags
             csp_meta_tag
 
+            link(rel: "icon", href: "/favicon.ico", sizes: "any")
+            link(rel: "icon", href: "/icon.svg", type: "image/svg+xml")
+            link(rel: "apple-touch-icon", href: "/apple-touch-icon.png")
             link(rel: "manifest", href: pwa_manifest_path)
             stylesheet_link_tag("application", media: "all")
             javascript_include_tag("application")
