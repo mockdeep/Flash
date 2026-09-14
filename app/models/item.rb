6920c3c0e0293b8f9968c62a8d15e8e8dd2d5b6a
Robert Fletcher <lobatifricha@gmail.com>
2026-09-13 17:28:38 -0700

    Compendium: drop pairings table (#448)

--- app/models/item.rb
+++ app/models/item.rb
@@ -5,9 +5,6 @@ class Item < ApplicationRecord
   belongs_to :entry
   has_many :cards, dependent: :destroy
 
-  has_many :pairings, dependent: :destroy
-  has_many :paired_items, through: :pairings, source: :paired_item
-
   has_many :item_distractors, dependent: :destroy
   has_many :distractors, through: :item_distractors, source: :distractor_item
 
--- spec/models/pairing_spec.rb
+++
@@ -1,8 +0,0 @@
-# frozen_string_literal: true
-
-require "rails_helper"
-
-RSpec.describe Pairing do
-  it { is_expected.to belong_to(:item) }
-  it { is_expected.to belong_to(:paired_item).class_name("Item") }
-end
